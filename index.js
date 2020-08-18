039b45e519a1348d5b8f7878a0938af1974d0200
claudiahalip <claudia4ro@yahoo.com>
2020-08-18 15:37:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your functions here
+function map(sourceArr, theFunction){
+    const newA = []
+    for(let elem of sourceArr){
+        newA.push(theFunction(elem))
+    }
+    return newA
+};
+
+function reduce(sourceArr, theFunction, initialValue = true ){
+    if (initialValue){
+    let accum = initialValue;
+    for(let elem of sourceArr){
+        accum = theFunction(elem, accum);
+    }
+    return accum
+
+    } else {
+        let accum = 0;
+        for(let elem of sourceArr){
+        accum = theFunction(elem, accum);
+        return accum ;}
+    }
+};
+
+
+
+
+
